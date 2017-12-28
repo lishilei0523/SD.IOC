415d3f53fb86a65e0abe3a8a463c2b3d1eb7df1f
Lee <lishilei0523@163.com>
2017-12-28 11:03:42 +0800

    整理项目结构

---
+++ tests/SD.IOC.Integration.WebApi.Tests/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SD.IOC.Integration.WebApi.Tests.WebApiApplication" Language="C#" %>
---
+++ tests/SD.IOC.Integration.WebApi.Tests/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Http;
+
+namespace SD.IOC.Integration.WebApi.Tests
+{
+    public class WebApiApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+        }
+    }
+}
