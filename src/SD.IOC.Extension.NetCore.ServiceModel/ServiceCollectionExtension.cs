f1c7998ff8ad2361adc3b1c94092360d12c36e5a
Lee <lishilei0523@163.com>
2018-04-27 14:35:21 +0800

    .NET Core WCF扩展整理

--- src/SD.IOC.Extension.NetCore.ServiceModel/ServiceCollectionExtension.cs
+++ src/SD.IOC.Extension.NetCore.ServiceModel/ServiceCollectionExtension.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.ServiceModel.Extensions;
+using System.ServiceModel.NetCore.Toolkits;
 
 namespace SD.IOC.Extension.NetCore.ServiceModel
 {
