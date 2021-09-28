caf541eefcff12c82714f59755f1736aa4296c92
Lee <lishilei0523@163.com>
2021-09-28 18:09:44 +0800

    更新nuget包；

--- src/SD.IOC.Extension.NetCore.ServiceModel/ServiceCollectionExtension.cs
+++ src/SD.IOC.Extension.NetCore.ServiceModel/ServiceCollectionExtension.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.ServiceModel.NetCore;
+using System.ServiceModel;
 
 namespace SD.IOC.Extension.NetCore.ServiceModel
 {
