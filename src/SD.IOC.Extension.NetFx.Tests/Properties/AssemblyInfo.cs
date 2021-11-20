bb3f6575612ee964d547a2e3ba7aecc6a4d764bd
Lee <lishilei0523@163.com>
2021-11-21 01:08:46 +0800

    整理文件；

--- src/SD.IOC.Extension.NetFx.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("a73448b8-196d-4311-8bb1-b729877233a5")]
