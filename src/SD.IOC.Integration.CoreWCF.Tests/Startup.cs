05472ee25ebfe38a82d7fc25fb7e6525107fdf7b
Lee <lishilei0523@163.com>
2022-05-02 18:36:02 +0800

    更新nuget包；

--- src/SD.IOC.Integration.CoreWCF.Tests/Startup.cs
+++ src/SD.IOC.Integration.CoreWCF.Tests/Startup.cs
@@ -1,8 +1,11 @@
 using CoreWCF.Configuration;
+using CoreWCF.Description;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using SD.Common;
 using SD.IOC.Integration.WCF.Behaviors;
 using SD.IOC.Integration.WCF.Tests.Implements;
+using System.Collections.Generic;
 using System.Configuration;
 
 namespace SD.IOC.Integration.WCF.Tests
@@ -17,8 +20,11 @@ namespace SD.IOC.Integration.WCF.Tests
         /// </summary>
         public void ConfigureServices(IServiceCollection services)
         {
+            //添加WCF服务
             services.AddServiceModelServices();
+            services.AddServiceModelMetadata();
 
+            //添加WCF配置
             Configuration configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
             services.AddServiceModelConfigurationManagerFile(configuration.FilePath);
         }
@@ -29,14 +35,19 @@ namespace SD.IOC.Integration.WCF.Tests
         public void Configure(IApplicationBuilder appBuilder)
         {
             //配置WCF服务
+            ServiceMetadataBehavior metadataBehavior = appBuilder.ApplicationServices.GetRequiredService<ServiceMetadataBehavior>();
+            metadataBehavior.HttpGetEnabled = true;
+            metadataBehavior.HttpsGetEnabled = true;
+            UseRequestHeadersForMetadataAddressBehavior addressBehavior = new UseRequestHeadersForMetadataAddressBehavior();
             DependencyInjectionBehavior dependencyInjectionBehavior = new DependencyInjectionBehavior();
+            IList<IServiceBehavior> serviceBehaviors = new List<IServiceBehavior>
+            {
+                addressBehavior, dependencyInjectionBehavior
+            };
 
             appBuilder.UseServiceModel(builder =>
             {
-                builder.ConfigureServiceHostBase<ProductService>(host =>
-                {
-                    host.Description.Behaviors.Add(dependencyInjectionBehavior);
-                });
+                builder.ConfigureServiceHostBase<ProductService>(host => host.Description.Behaviors.AddRange(serviceBehaviors));
             });
         }
     }
